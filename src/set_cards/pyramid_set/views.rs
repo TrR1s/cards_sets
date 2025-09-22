3b94e219e0a8887cc26633b4b03d2b4dfa10175e
TrR1s <anatolyturin0505@gmail.com>
2025-09-22 12:12:26 +0300

    pyr diplay rev

--- src/set_cards/pyramid_set/views.rs
+++ src/set_cards/pyramid_set/views.rs
@@ -1,4 +1,4 @@
-use std::fmt; 
+use std::{fmt}; 
 use  crate::card::card_analog::{Rank};
 use super::PyramidSet;
 use crate::set_cards::FLUSHMASK64;
@@ -40,9 +40,12 @@ impl PyrVew {
 impl fmt::Display for PyrVew {
     fn fmt(&self, f:&mut fmt::Formatter)->fmt::Result{
         let flush_set:String = if self.flush_is {"One Suit".to_string()} else {"UnSuit".to_string()} ;
-        let mut pyr_str = String::from("\n");
+        
+
+        let mut str_vec : Vec<String> = vec![];
         for curr_level in self.pyr_arr{
             let mut ind =2;
+            let mut pyr_str = String::from("\n");
             for curr_el in curr_level{
                 let curr_str = match curr_el {
                     Some(rank) => rank.short_string(),
@@ -53,10 +56,14 @@ impl fmt::Display for PyrVew {
                 pyr_str.push_str(curr_str);
                 pyr_str.push_str("_");
             }
-            pyr_str.push_str("\n");
+            str_vec.push(pyr_str);
 
         }
-        let res_str =  format!("***{flush_set}***,\n {pyr_str}");
+        let mut res_str = String::new();
+        for i in (0..=3).rev(){
+            res_str.push_str(str_vec.get(i).unwrap());
+        }
+        let res_str =  format!("***{flush_set}***, {res_str}");
         write!(f,"{res_str}")
     }
     
