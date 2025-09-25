fa34d3404919b40f2b8e3a87e4e551f69b4a7374
TrR1s <anatolyturin0505@gmail.com>
2025-09-25 13:09:57 +0300

    clippy for ... fix

--- src/set_cards/flat_set/views.rs
+++ src/set_cards/flat_set/views.rs
@@ -8,7 +8,8 @@ impl fmt::Display for FlatSet {
         // let mut one_suit_vec:Vec<String> = vec![String::from("__");13];
         let mut card_arr_str:Vec<Vec<String>> = vec![vec![String::from("__");13];4];
 
-        for i in 0..4{ card_arr_str[i][10]=String::from("___");}
+        // for i in 0..4{ card_arr_str[i][10]=String::from("___");}
+        for vec_suit in card_arr_str.iter_mut(){vec_suit[10]=String::from("___");}
 
         let vec_card = self.flat_to_vec_cards();
         for curr_card in vec_card {
@@ -17,8 +18,13 @@ impl fmt::Display for FlatSet {
             card_arr_str[curr_card.suit.suit_number() as usize][curr_card.rank.rank_number()as usize]= val_str; 
         }
         let mut res_str=String::from("\n");
-        for i in 0..4{
-            let res_str_curr = card_arr_str[i].join(",");
+        // for i in 0..4{
+        //     let res_str_curr = card_arr_str[i].join(",");
+        //     res_str.push_str(res_str_curr.as_str());
+        //     res_str.push('\n');
+        // }
+        for vec_suit in card_arr_str.iter(){
+            let res_str_curr = vec_suit.join(",");
             res_str.push_str(res_str_curr.as_str());
             res_str.push('\n');
         }
--- src/set_cards/utils/flat_to_pyr.rs
+++ src/set_cards/utils/flat_to_pyr.rs
@@ -5,31 +5,63 @@ let mut bin_freq:[u8;13]=[0;13];// amount cards by rank in the set
     let mut pyramid_cb: u64=0;
     let mut flat_copy = flat_64;
     while flat_copy != 0 {
-        for curr_rank in 0..13{
+
+
+        // for curr_rank in 0..13{
+        //     if flat_copy & 1 == 1 {
+        //         bin_freq[curr_rank] += 1; 
+        //     }
+        //     flat_copy >>=1
+        // } 
+
+
+        for rank_val in &mut bin_freq{
             if flat_copy & 1 == 1 {
-                bin_freq[curr_rank] += 1; 
+                *rank_val += 1; 
             }
             flat_copy >>=1
         } 
+
+
+
         flat_copy >>= 3;
     }
 
-    for curr_rank in 0..13{
-        if bin_freq[curr_rank] == 0 {
+    // for curr_rank in 0..13{
+    //     if bin_freq[curr_rank] == 0 {
+    //         continue;
+    //     }
+    //     let curr_rank_bin_mask: u64 = 1<<curr_rank;
+    //     let mut curr_rank_bin: u64 = curr_rank_bin_mask;
+    //     bin_freq[curr_rank] -=1;
+    //     while bin_freq[curr_rank] != 0 {
+    //         curr_rank_bin <<= 16;
+    //         curr_rank_bin |= curr_rank_bin_mask;
+    //         bin_freq[curr_rank] -= 1;
+    //     }
+    //     pyramid_cb |= curr_rank_bin;
+
+
+    // }
+
+    for (curr_rank,rank_val) in bin_freq.iter_mut().enumerate(){
+        if *rank_val == 0 {
             continue;
         }
         let curr_rank_bin_mask: u64 = 1<<curr_rank;
         let mut curr_rank_bin: u64 = curr_rank_bin_mask;
-        bin_freq[curr_rank] -=1;
-        while bin_freq[curr_rank] != 0 {
+        *rank_val -=1;
+        while *rank_val != 0 {
             curr_rank_bin <<= 16;
             curr_rank_bin |= curr_rank_bin_mask;
-            bin_freq[curr_rank] -= 1;
+            *rank_val -= 1;
         }
         pyramid_cb |= curr_rank_bin;
 
 
     }
+
+
 
     // check for flush
     let mut suit_mask: u64 = 0b0001_1111_1111_1111;
