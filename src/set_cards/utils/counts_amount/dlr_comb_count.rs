dbefebace510af0095d785eb611f41ce4f212390
TrR1s <anatolyturin0505@gmail.com>
2025-10-10 10:52:59 +0300

    diff doesnt cross to virt

--- src/set_cards/utils/counts_amount/dlr_comb_count.rs
+++ src/set_cards/utils/counts_amount/dlr_comb_count.rs
@@ -46,7 +46,7 @@ pub fn dlr_comb_count(fix_cards:&FixMix,deck_info:&DeckInfo,comb5_unfl_pyr:Pyram
 
     //3
     // count total amount comb5 minus ex ???? стоит ли вычитать?
-    let comb5_unfl_amount = pyr_in_pyr_flush_free(pyr_diff, deck_info.pyr_after) - deck_info.virt_cards.fl_except_amount ;
+    let comb5_unfl_amount = pyr_in_pyr_flush_free(pyr_diff, deck_info.pyr_after) ;
 
     // 4
     // если флеш не возможен. 
@@ -64,6 +64,55 @@ pub fn dlr_comb_count(fix_cards:&FixMix,deck_info:&DeckInfo,comb5_unfl_pyr:Pyram
 
 
 
+    // 5
+    // Флешы возможны.
+
+    // проверяем, пересекается ли наш diff_pyr с virt_cards, если не пересекается, то 
+    // то делаем по принципу exchange_comb_count
+
+    // let diff_cross_virt = deck_info.virt_cards.virt_pyr & pyr_diff == 0; // True if not crosses
+    let check_flush= |flat64:FlatSet64,pyr_fl_64:PyramidSet64, suit_n:u8| -> (i64,FlatSet64) {
+        
+        let flush_n_64 = pyr_fl_64 << 16*suit_n;
+        if flush_n_64 == flush_n_64 & flat64 {
+            return (1,flush_n_64);
+        }
+        (0,0)}; 
+    
+    // не пересекается:
+    if deck_info.virt_cards.virt_pyr & pyr_diff == 0 as u64 // True if not crosses
+
+                {
+                    
+                let (comb5_fl_amount,fl_flat)  = match  fix_cards.suitable_info {
+                    FixSuitable::NOT => (0,0),
+                    FixSuitable::YES(suit_n) => check_flush(deck_info.deck_before.flat,pyr_diff,suit_n),
+                    FixSuitable::ANY => {
+                            let mut fl_amount: i64 = 0;
+                            let mut fl_flat: PyramidSet64 = 0;
+                            for suit_r in [0,1,2,3]{
+                                let (curr_fl_amount, curr_fl_flat) = check_flush(deck_info.deck_before.flat,pyr_diff,suit_r);
+                                fl_amount += curr_fl_amount;
+                                fl_flat |= curr_fl_flat;
+
+                            }
+                            (fl_amount,fl_flat)
+
+                                        }
+                    };
+
+                return ((comb5_unfl_amount-comb5_fl_amount) as f64,FlushInfo{
+                                    comb_fl_is:comb5_fl_exist,
+                                    amount:comb5_fl_amount,
+                                    fl_flat:fl_flat},
+                                 VirtCards{
+                                    virt_pyr:pyr_diff,
+                                    virt_flush_except_flat:fl_flat,
+                                    fl_except_amount:comb5_fl_amount
+                                });
+
+                };
+    // пересекается:
 
 
 
